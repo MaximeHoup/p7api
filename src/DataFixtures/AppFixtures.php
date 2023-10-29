7923f09815dd2c1c168f2d0f1dcd95b2a10606ca
Maxime Houpiarpanin <m.houpiarpanin@gmail.com>
2023-10-29 12:40:11 +0100

    adding jwt authentication

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,9 +7,17 @@ use App\Entity\Phone;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         for ($i = 1; $i <= 10; $i++) {
@@ -21,14 +29,21 @@ class AppFixtures extends Fixture
         }
 
         $listclient = [];
-        for ($i = 1; $i <= 3; $i++) {
-            // Création de l'auteur lui-même.
-            $client = new Client();
-            $client->setName('Client n°' . $i);
-            $manager->persist($client);
-            // On sauvegarde l'auteur créé dans un tableau.
-            $listclient[] = $client;
-        }
+        $client = new Client();
+        $client->setName('Client');
+        $client->setEmail('client@mail.fr');
+        $client->setPassword($this->userPasswordHasher->hashPassword($client, "password"));
+        $client->setRoles(["ROLE_USER"]);
+        $manager->persist($client);
+        $listclient[] = $client;
+
+        $clientAdmin = new Client();
+        $clientAdmin->setName('Client admin');
+        $clientAdmin->setEmail('clientadmin@mail.fr');
+        $clientAdmin->setPassword($this->userPasswordHasher->hashPassword($clientAdmin, "password"));
+        $clientAdmin->setRoles(["ROLE_ADMIN"]);
+        $manager->persist($clientAdmin);
+        $listclient[] = $clientAdmin;
 
         for ($i = 1; $i <= 10; $i++) {
             $user = new User();
